b5a4e840318e7094af422a5c714765747517cf81
Thiago Volpi Rabethge <89933443+ThiagoVRabethge@users.noreply.github.com>
2023-04-07 20:15:15 +0000

    fix server js

--- services/server.js
+++ services/server.js
@@ -1,7 +1,11 @@
 import axios from "axios";
 
+// const server = axios.create({
+//   baseURL: "http://localhost:3000/api",
+// });
+
 const server = axios.create({
-  baseURL: "http://localhost:3000/api",
+  baseURL: process.env.REACT_BASE_URL,
 });
 
 export default server;
