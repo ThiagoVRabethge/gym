1e599771a818ff811662e3a5b7402fb5312fcc10
Thiago Volpi Rabethge <89933443+ThiagoVRabethge@users.noreply.github.com>
2023-04-07 15:55:11 +0000

    add dot env to deploy

--- pages/api/db.js
+++ pages/api/db.js
@@ -1,11 +1,19 @@
 import { Pool } from "pg";
 
+// const pool = new Pool({
+//     user: 'postgres',
+//     host: 'db.jfromkdlhhwdtyhsnivz.supabase.co',
+//     database: 'postgres',
+//     password: 'QgnBV4thi4A8aUPE',
+//     port: 5432,
+// });
+
 const pool = new Pool({
-    user: 'postgres',
-    host: 'db.jfromkdlhhwdtyhsnivz.supabase.co',
-    database: 'postgres',
-    password: 'QgnBV4thi4A8aUPE',
-    port: 5432,
+  user: process.env.PG_USER,
+  host: process.env.PG_HOST,
+  database: process.env.PG_DB,
+  password: process.env.PG_PWD,
+  port: process.env.PG_PORT,
 });
 
 module.exports = {
