2849efadbc308aed54928a6b7ee364f684557592
Thiago Volpi Rabethge <89933443+ThiagoVRabethge@users.noreply.github.com>
2023-04-07 21:03:48 +0000

    fix list workout

--- pages/workouts/Workouts.js
+++ pages/workouts/Workouts.js
@@ -12,7 +12,7 @@ const Workouts = () => {
 
   const GetWorkouts = () => {
     server
-      .get("https://gym-ekjf96cyi-thiagovrabethge.vercel.app/api/listWorkouts")
+      .get("https://gym-nu-lyart.vercel.app/api/listWorkouts")
       .then((response) => {
         setWorkoutsList(response.data);
       });
